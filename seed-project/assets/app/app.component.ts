f0dcf4e629b697fa74ceaef78c9eda214ba9f59a
Castiel <castiel.andrew@gmail.com>
2018-03-15 23:23:06 -0700

    added message-input and message-list component, able to print user input to console

--- seed-project/assets/app/app.component.ts
+++ seed-project/assets/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { Message } from './messages/message.model'
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Max'),
-        new Message('3 rd Some message', 'Max'),
-        new Message('Some other message', 'Max')
-    ];
 }
---
+++ seed-project/assets/app/messages/message-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { Message } from "./message.model";
+
+@Component({
+    selector: 'app-message-input',
+    templateUrl: './message-input.component.html'
+})
+export class MessageInputComponent {
+    onSave(value: string) {
+        console.log(value);
+    }
+}
