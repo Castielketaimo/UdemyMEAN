14f2b09ceb185a005b70c0116202d97da8804568
Castiel <castiel.andrew@gmail.com>
2018-03-15 23:48:40 -0700

    able to add data though service

--- seed-project/assets/app/messages/message-input.component.ts
+++ seed-project/assets/app/messages/message-input.component.ts
@@ -1,12 +1,17 @@
 import { Component } from "@angular/core";
 import { Message } from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html'
+    templateUrl: './message-input.component.html',
+    providers: [MessageService]
 })
 export class MessageInputComponent {
+    constructor(private messageService: MessageService){}
+
     onSave(value: string) {
-        console.log(value);
+        const message = new Message(value, 'Max');
+        this.messageService.addMessage(message);
     }
 }
