45d7ada3fca492becf7c6202227e4bd51c653f09
Castiel <castiel.andrew@gmail.com>
2018-03-15 22:48:44 -0700

    Added Property Binding & event binding

---
+++ seed-project/assets/app/messages/message.model.ts
@@ -0,0 +1,15 @@
+import { user } from "../auth/user.model";
+
+export class Message {
+    content: string;
+    username: string;
+    messageId?: string;
+    userId?: string;
+
+    constructor(content: string, username: string, messageId?: string, userId?: string){
+        this.content = content;
+        this.username = username;
+        this.messageId = messageId;
+        this.userId = userId;
+    }
+}
